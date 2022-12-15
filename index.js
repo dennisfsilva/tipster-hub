7b532cdd8b49e7c060398e028a0cf04b50b0728f
Daniel Silva <daniel.silva.prg@gmail.com>
2022-12-15 19:56:53 +0000

    add of dependencies and test of basic examples. must think of auto request log manager

--- index.js
+++ index.js
@@ -9,17 +9,10 @@ app.use(express.json())
 
 // API services request functionalities (GET/POST/PUT/DELETE/UPDATE ...) 
 app.get('/home', function (req, res) {
-      	data = {
-	"test0":{
-		"name": "botclimber",
-		"music": "trap",
-		"interests": "makeup"
-		}
-	}
 	console.log( "request to /home");
-      	res.end(JSON.stringify());
+	res.status(200).json("stop trying be god")
    });
-})
+
 
 app.listen(port, () => {
 	console.log('Server started on port ' + port)
