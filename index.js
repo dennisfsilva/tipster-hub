f2a3ce6a5738002de89e3a055671127f0bf9016a
Daniel Silva <daniel.silva.prg@gmail.com>
2022-12-15 19:46:00 +0000

    wip: some example

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+require('dotenv').config();
+const port = process.env.PORT || 8000; 
 
-var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+const express = require('express');
+const app = express();
+
+// use json expressions
+app.use(express.json())
+
+// API services request functionalities (GET/POST/PUT/DELETE/UPDATE ...) 
+app.get('/home', function (req, res) {
+      	data = {
+	"test0":{
+		"name": "botclimber",
+		"music": "trap",
+		"interests": "makeup"
+		}
+	}
+	console.log( "request to /home");
+      	res.end(JSON.stringify());
+   });
 })
+
+app.listen(port, () => {
+	console.log('Server started on port ' + port)
+});
